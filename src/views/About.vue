99063c760e7154d210ffd59cf233be6c8c008509
jimoruyan <354065853@qq.com.com>
2020-01-04 16:15:32 +0800

    element-ui

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld />
   </div>
 </template>
+
+<script>
+
+import HelloWorld from '../components/HelloWorld'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
